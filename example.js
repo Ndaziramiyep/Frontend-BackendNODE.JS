2245795696c03bcdcbbfa105e39dc49fe2ba99d1
Ndaziramiyep <patrickofficial30@gmail.com>
2024-05-25 20:00:06 +0200

    learning frontend and backend

--- example.js
+++ example.js
@@ -1,1 +1,17 @@
-require("./learning");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname,"welcome.html"))
+})
+app.get("/data/database",(req,res) => {
+    const members = [{Id:"001", Name:"Patrick NDAZIRAMIYE", Salary:"1M", Age:"20"},
+    {Id:"002", Name:"Patrick NDAZIRAMIYE1", Salary:"2M", Age:"30"},
+    {Id:"003", Name:"Patrick NDAZIRAMIYE3", Salary:"3M", Age:"40"},
+    ]
+    res.json(members);
+})
+app.listen(3000,()=>{
+    console.log("server is running on localhost:3000");
+})
--- learning.js
+++ learning.js
@@ -1,16 +1,24 @@
 const express= require("express");
 const logger = require("./middlewares/middlewareTest");
 const app = express();
+const testing = require("./register")
 app.get("/", (req,res,a) =>{
     res.send("Welcome Here!")
     a();
 })
-
+app.use("views","ej engine");
 app.use(logger);
+app.use(testing);
+
 
 app.get("/users", (req,res) =>{
     console.log("This is middleware Test!")
     res.send("Users and logged in")
+})
+
+app.get("/admin", (req,res) =>{
+    console.log("This is middleware Test!")
+    res.send("Admin and logged in")
 })
 
 const port=3000;
