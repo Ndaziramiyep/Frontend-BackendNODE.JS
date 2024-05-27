6e9b0221e3490b2dce82b7fba181dec8e8f4633c
Ndaziramiyep <patrickofficial30@gmail.com>
2024-05-27 20:26:55 +0200

    Learning about controllers

--- example.js
+++ example.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 5000;
-
 const app = express();
+require('dotenv').config();
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
+
 app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname,"welcome.html"))
 })
