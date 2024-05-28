c4f16468a077f922975f85b2b1a02623c8e673c9
Ndaziramiyep <patrickofficial30@gmail.com>
2024-05-28 19:27:59 +0200

    Adding middleware for post method

--- app.js
+++ app.js
@@ -3,5 +3,16 @@ const bodyparser = require('body-parser');
 
 
 const app = express();
+app.set('views engine', 'ejs')
 
-app.use(express.urlencoded)
+app.use(bodyparser.urlencoded({extended: true}));
+let tasks = [];
+app.get('/', (req, res) => {
+    res.render('home',{tasks:tasks});
+})
+
+app.post('/add',(req,res) =>{
+    const {task} = req.body;
+    tasks.push(task);
+    res.render('/')
+})
