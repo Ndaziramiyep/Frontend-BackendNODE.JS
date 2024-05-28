81d11c223e88a52bb5f8e65188298064ed428e29
Ndaziramiyep <patrickofficial30@gmail.com>
2024-05-28 20:41:44 +0200

    creation of database

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const mysql = require('mysql');
+
+
 
 
 const app = express();
 const port = process.env.PORT || 5000;
-app.set('view engine', 'ejs')
+require('dotenv').config;
+app.set('view engine', 'ejs');
+
+const conn=mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"gia"
+  });
+
+conn.connect((err) =>{
+    if(!err){
+        console.log("Connection Successfully!")
+    }
+    else{
+        console.error("Error occurred "+ err);
+    }
+})
 
 
 app.use(bodyparser.urlencoded({extended: true}));
