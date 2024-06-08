eacf029f329673df606ef0d1d4c32f7254bf901e
Ndaziramiyep <patrickofficial30@gmail.com>
2024-06-08 20:06:38 +0200

    Getting sessions

---
+++ session.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const session = require('express-session');
+
+const app = express();
+const port = 3000;
+
+// Set up the session middleware
+app.use(session({
+    secret: 'your_secret_key', // Secret key for signing the session ID cookie
+    resave: false, // Do not save session if unmodified
+    saveUninitialized: true, // Save uninitialized sessions
+    cookie: { secure: false } // Set to true if using HTTPS
+}));
+
+// Route to set a session variable
+app.get('/set-session', (req, res) => {
+    req.session.username = 'JohnDoe';
+    res.send('Session variable set: username = JohnDoe');
+});
+
+// Route to get the session variable
+app.get('/get-session', (req, res) => {
+    if (req.session.username) {
+        res.send(`Session variable value: username = ${req.session.username}`);
+    } else {
+        res.send('No session variable found');
+    }
+});
+
