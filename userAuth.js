ee2164291f22776ab80b7d0e208133ec0157e5e9
Ndaziramiyep <patrickofficial30@gmail.com>
2024-06-09 06:31:52 +0200

    Middleware to check if user is authenticated

--- userAuth.js
+++ userAuth.js
@@ -13,3 +13,28 @@ app.use(session({
     cookie: { maxAge: 60000 } // 1-minute session for demo purposes
 }));
 
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'example_db'
+});
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to database');
+});
+
+// Middleware to check if user is authenticated
+const isAuthenticated = (req, res, next) => {
+    if (req.session.userId) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+};
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
